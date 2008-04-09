8adb4967951ad6f4f088b6974f02ce998999110f
Jonas Dorn <jonas.dorn@gmail.com>
2008-04-09 22:24:46 +0000

    sec2struct.m: aesthetic fix
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/extern/main@2780 0bcb941b-74d1-0310-bb26-e6636707f451

--- sec2struct.m
+++ sec2struct.m
@@ -21,3 +21,4 @@ time_strct.seconds = floor(mod(x,60));
 time_strct.miliseconds = mod(x,1)*1e3;
 
 time_strct.str = sprintf('%02d:%02d:%02d.%3.0f', time_strct.hour, time_strct.minutes, time_strct.seconds, time_strct.miliseconds);
+time_strct.str = regexprep(time_strct.str,' ','0');
