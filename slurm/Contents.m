a6bbc549d9bf466c0cea094bcba015f52a28d1b5
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2016-10-04 22:24:43 -0500

    slurm: Add alternate communicatingSubmitFcn.m in +nomdcs package for single node slurm jobs

---
+++ slurm/Contents.m
@@ -0,0 +1,9 @@
+% This folder contains scripts to assist with integration of MATLAB with the cluster scheduler
+% Slurm (Simple Linux Utility for Resource Management).
+%
+% Contents:
+% +nomdcs : This package contains a script to modify the slurm cluster profile such that single
+%         node jobs do not checkout MDCS licenses. NOT LICENSED FOR REDISTRIBUTION.
+%
+% Change Log
+% 2016_10_04: Mark Kittisopikul created the folder and added +nomdcs
