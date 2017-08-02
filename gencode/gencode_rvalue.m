04a338412a51f9034bc5ccaf7de4881acb06ed05
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2017-08-02 18:43:25 -0500

    gencode: textscan no longer accepts bufsize as a parameter in R2016b

--- gencode/gencode_rvalue.m
+++ gencode/gencode_rvalue.m
@@ -114,7 +114,9 @@ switch class(item)
                     sitem = mat2str(item,'class');
                 end
                 bsz   = max(numel(sitem)+2,100); % bsz needs to be > 100 and larger than string length
-                str1 = textscan(sitem, '%s', 'delimiter',';', 'bufsize',bsz); 
+                % mkitti: August 2nd, 2017 bufsize is no longer a valid parameter
+%                 str1 = textscan(sitem, '%s', 'delimiter',';', 'bufsize',bsz); 
+                str1 = textscan(sitem, '%s', 'delimiter',';'); 
                 if numel(str1{1}) > 1
                     str = str1{1};
                 else
